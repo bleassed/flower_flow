29a20bdf1b3369cc1ddc9da159a2c2fcd3f3e2d1
Балакирев Алексей Викторович <ABalakirev@synergy.ru>
2026-04-03 18:04:42 +0300

    first commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+
+    <div class = "wrapper">
+
+        <header>
+
+        <div class = "header_blok">
+            <div class = "header_items">
+
+                <ul class="nav">
+                    @yield('navigate')
+                </ul>
+
+
+
+
+
+        </div>
+
+
+        </header>
+
+
+        <main>
+
+        @yield('main')
+
+        </main>
+
+        <footer>
+
+
+        </footer>
+
+
+
+
+
+
+
+
+    </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ resources/views/main_page.blade.php
@@ -0,0 +1,26 @@
+@extends('layout.layout')
+
+
+@section('title', 'home')
+
+@section('main')
+
+<p>Hellloooo!!! It's me!!</p>
+
+@endsection
+
+
+@section('navigate')
+
+    <li class="nav_item">
+        <a class="nav-link active" aria-current="page" href="/">home</a>
+    </li>
+    <li class="nav_item">
+        <a class="nav-link" href="#">////</a>
+    </li>
+    <li class="nav_item">
+        <a class="nav-link" href="#">////</a>
+    </li>
+
+
+@endsection
